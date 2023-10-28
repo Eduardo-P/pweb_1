df9554ef18b752dd03efbb66f57f3d2d4f55f56a
Eduardo Portugal Portugal <eportugalpor@unsa.edu.pe>
2023-10-28 18:54:23 -0500

    Creando el script con Perl, esta primera version modifica, actualiza y redirecciona al HTMl de la calculadora, la modificacion se realiza en el TextBox

---
+++ cgi-bin/calculadora.pl
@@ -0,0 +1,26 @@
+#!"C:/xampp/perl/bin/perl.exe"
+use strict;
+use warnings;
+use CGI;
+
+my $cgi = CGI->new;
+
+my $operacion = $cgi->param('operacion');
+
+open my $archivoHTML, '<', '../htdocs/Calculadora.html';
+my @archivoHTML = <$archivoHTML>;
+close $html_file;
+
+for my $line (@archivoHTML) {
+    if ($line =~ /<input type="text" name="operacion" value='/) {
+        $line =~ s/(<input type="text" name="operacion" value=')\S*('>)/$1$operacion$2/;
+    }
+}
+
+
+open my $archivoHTML, '>', '../htdocs/Calculadora.html';
+print $archivoHTML @archivoHTML;
+close $archivoHTML;
+
+
+print $cgi->redirect('http://localhost/Calculadora.html');
